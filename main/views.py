361e42bf363dcc8ab76e8f819f3a677c9cf9f732
manishReddevil <manishojhac3@gmail.com>
2023-08-02 22:20:03 +1000

    Adding pagination to the query

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.db.models import Avg, F, Q, Value, IntegerField, Count
 from main.models import SensorEvent, Sensor
 from .serializers import TrafficSerializer
 from django.db.models.functions import Coalesce
+from rest_framework.pagination import PageNumberPagination
 
 @api_view(['GET'])
 def day_of_week_average_count(request):
@@ -13,19 +14,32 @@ def day_of_week_average_count(request):
     if not start_date or not end_date:
         return Response({"error": "Both start_date and end_date parameters are required."}, status=400)
 
-    # Assuming you have a 'event_datetime' field in the SensorEvent model
+   
     # to store the date and time of each event
     queryset = Sensor.objects.filter(sensor_events__event_datetime__range=[start_date, end_date]).annotate(
         sensor_id = F('id'),
         sensor_name = F('name'),
-    ).values('sensor_id', 'sensor_name')[:1000]
+    ).values('sensor_id', 'sensor_name')
 
+
+    paginator = PageNumberPagination()
+
+    paginator.page_size = 50  # Setting the number of items per page
+
+    # Paginating the queryset based on the request's GET parameters
+    paginated_queryset = paginator.paginate_queryset(queryset, request)
+
+    print("Queryset:", queryset)
+    print("Paginated queryset:", paginated_queryset)
+
+    if not paginated_queryset:  # Checking if paginated_queryset is empty
+        return Response({"error": "No data available for the given date range or page number."}, status=404)
     reports = []
+
     # if not queryset.exists():
     #     return Response({"error": "No data available for the given date range."}, status=404)
-    for q in queryset:
-
-    # Calculate the average count for each day of the week (Monday to Sunday)
+    for q in paginated_queryset:
+    # Average count for each day of the week (Monday to Sunday)
         averages = SensorEvent.objects.filter(event_datetime__range=[start_date, end_date], sensor__id=q['sensor_id']).values('sensor__id').annotate(
         
             mon_count= Coalesce(
@@ -80,7 +94,7 @@ def day_of_week_average_count(request):
             sun_avg_count = Avg(F('sun_count')),
         )
         reports.append({**q,**averages})
-    print(reports)
+
     serializer = TrafficSerializer(reports, many=True)
 
-    return Response({"results": serializer.data})
+    return paginator.get_paginated_response(serializer.data)
